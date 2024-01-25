2d59fc0c998ba2859b4769ea7e151e9aafc7d012
GabeLawFR <gabriel.lawson@live.fr>
2024-01-25 15:40:51 -0800

    init button and pageheader foundation

--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -1,5 +1,19 @@
+import { Menu } from 'lucide-react'
+import logo from '../assets/logo.png'
+import Button from '../Components/Button'
+
+
 export default function PageHeader() {
     return (
-        <div></div>
+        <div className="flex gap-10 lg:gap-20 justify-between">
+            <div className="flex gap-4 items-center flex-shrink-0">
+                <Button>
+                    <Menu />
+                </Button>
+                <a href="/">
+                    <img src={logo} alt="" className="h-6"/>
+                </a>
+            </div>
+        </div>
     )
 }
